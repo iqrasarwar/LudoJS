02f694a837cedbd92fd952dd6cfd962dfe60329b
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2021-12-31 15:37:34 +0500

    ALL DICES CIRCULATING
    this version contain code where all 16 dices are circulating on correct path until they have to go home.(go home isn't working yet)

--- script.js
+++ script.js
@@ -1,330 +1,138 @@
 let diceRollResults = 0;
-let redPos = [43,43,43,43];
-let bluePos = [30,30,30,30];
-let greenPos = [4,4,4,4];
-let yellowPos = [17,17,17,17];
-let Turn = [false,false,false,false,false];
-let blueDicesStatus = [false,false,false,false];
-let redDicesStatus = [false,false,false,false];
-let yellowDicesStatus = [false,false,false,false];
-let greenDicesStatus = [false,false,false,false];
+let Pos = [30,30,30,30,43,43,43,43,4,4,4,4,17,17,17,17];
+let DicesStatus = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
 let DiceValues = ["0.png","1.png","2.png","3.png","4.png","5.png","6.png"];
-let blueDices = [document.getElementById("bdice1"),document.getElementById("bdice2"),document.getElementById("bdice3"),document.getElementById("bdice4")];
-let redDices = [document.getElementById("rdice1"),document.getElementById("rdice2"),document.getElementById("rdice3"),document.getElementById("rdice4")];
-let yellowDices = [document.getElementById("ydice1"),document.getElementById("ydice2"),document.getElementById("ydice3"),document.getElementById("ydice4")];
-let greenDices = [document.getElementById("gdice1"),document.getElementById("gdice2"),document.getElementById("gdice3"),document.getElementById("gdice4")];
+let Dices = [document.getElementById("bdice1"), document.getElementById("bdice2"), document.getElementById("bdice3"),document.getElementById("bdice4"), document.getElementById("rdice1"), document.getElementById("rdice2"), document.getElementById("rdice3"),document.getElementById("rdice4"), document.getElementById("gdice1"),document.getElementById("gdice2"),document.getElementById("gdice3"),document.getElementById("gdice4"), document.getElementById("ydice1"),document.getElementById("ydice2"),document.getElementById("ydice3"),document.getElementById("ydice4")];
 
 function DiceRoll()
 {
    diceRollResults = Math.floor((Math.random() * 6)+1);
    document.getElementById("RollBox").setAttribute("src", DiceValues[diceRollResults]);
    document.getElementsByClassName("DiceRoller")[0].onclick = false;
-   if(document.getElementById("rollText").innerText == "Player 1 Turn")
-      Turn[1]=true;
-   else if(document.getElementById("rollText").innerText == "Player 2 Turn")
-      Turn[2]=true;
-   else if(document.getElementById("rollText").innerText == "Player 3 Turn")
-      Turn[3]=true;
-   else if(document.getElementById("rollText").innerText == "Player 4 Turn")
-      Turn[4]=true;
-   startTransition();
+   startGame();
 }
-function startTransition()
+function startGame()
 {
-   let next = false;
-   if(Turn[1]==true)
+   let i = getIndex(document.getElementById("rollText").innerText);
+   let next = true;
+   for (let index = i; index < i+4; index++)
    {
-      next = false;
-      for (let index = 0; index < blueDices.length; index++)
+      if(DicesStatus[index]==false && diceRollResults == 6)
       {
-         if(blueDicesStatus[index]==false && diceRollResults == 6)
-         {
-            blueDices[index].onclick = freeDiceBlue;
-            next = true;
-         }
-         else if(blueDicesStatus[index]==true)
-         {
-            blueDices[index].onclick = transitDiceBlue;
-            next = true;
-         }
-         else
-         {
-            blueDices[index].onclick = false;
-         }
+         Dices[index].onclick = freeDice;
+         next = false;
       }
-      if(next == false)
+      else if(DicesStatus[index]==true)
       {
-         document.getElementById("rollText").innerText = "Player 2 Turn";
-         document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
-         Turn[1] = false;
-      }
-   }
-   if(Turn[2]==true)
-   {
-      next = false;
-      for (let index = 0; index < redDices.length; index++)
-      {
-         if(redDicesStatus[index]==false && diceRollResults == 6)
-         {
-            redDices[index].onclick = freeDiceRed;
-            next = true;
-         }
-         else if(redDicesStatus[index]==true)
-         {
-            redDices[index].onclick = transitDiceRed;
-            next = true;
-         }
-         else
-         {
-            redDices[index].onclick = false;
-         }
-      }
-      if(next == false)
-      {
-         document.getElementById("rollText").innerText = "Player 3 Turn";
-         document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
-         Turn[2] = false;
-      }
-   }
-   if(Turn[3]==true)
-   {
-      next = false;
-      for (let index = 0; index < greenDices.length; index++)
-      {
-         if(greenDicesStatus[index]==false && diceRollResults == 6)
-         {
-            greenDices[index].onclick = freeDiceGreen;
-            next = true;
-         }
-         else if(greenDicesStatus[index]==true)
-         {
-            greenDices[index].onclick = transitDiceGreen;
-            next = true;
-         }
-         else
-         {
-            greenDices[index].onclick = false;
-         }
-      }
-      if(next == false)
-      {
-         document.getElementById("rollText").innerText = "Player 4 Turn";
-         document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
-         Turn[3] = false;
-      }
-   }
-   if(Turn[4]==true)
-   {
-      next = false;
-      for (let index = 0; index < yellowDices.length; index++)
-      {
-         if(yellowDicesStatus[index]==false && diceRollResults == 6)
-         {
-            yellowDices[index].onclick = freeDiceYellow;
-            next = true;
-         }
-         else if(yellowDicesStatus[index]==true)
-         {
-            yellowDices[index].onclick = transitDiceYellow;
-            next = true;
-         }
-         else
-         {
-            blueDices[index].onclick = false;
-         }
-      }
-      if(next == false)
-      {
-         document.getElementById("rollText").innerText = "Player 1 Turn";
-         document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
-         Turn[4] = false;
-      }
-   }
-}
-function freeDiceBlue()
-{
-   let startPos = document.getElementById("b30");
-   let dice = event.target;
-   startPos.append(dice);
-   document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
-   for (let index = 0; index < blueDices.length; index++) {
-      if(dice == blueDices[index])
-      {
-         blueDicesStatus[index]=true;
-         console.log(dice);
+         Dices[index].onclick = transitDice;
+         next = false;
       }
       else
       {
-         blueDices[index].onclick = false;
+         Dices[index].onclick = false;
       }
    }
-}
-function freeDiceRed()
-{
-   let startPos = document.getElementById("b43");
-   let dice = event.target;
-   startPos.append(dice);
-   document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
-   for (let index = 0; index < redDices.length; index++) {
-      if(dice == redDices[index])
-      {
-         redDicesStatus[index]=true;
-         console.log(dice);
-      }
-      else
-      {
-         redDices[index].onclick = false;
-      }
-   }
-}
-function freeDiceYellow()
-{
-   let startPos = document.getElementById("b17");
-   let dice = event.target;
-   startPos.append(dice);
-   document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
-   for (let index = 0; index < yellowDices.length; index++) {
-      if(dice == yellowDices[index])
-      {
-         yellowDicesStatus[index]=true;
-         console.log(dice);
-      }
-      else
-      {
-         yellowDices[index].onclick = false;
-      }
-   }
-}
-function freeDiceGreen()
-{
-   let startPos = document.getElementById("b4");
-   let dice = event.target;
-   startPos.append(dice);
-   document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
-   for (let index = 0; index < greenDices.length; index++) {
-      if(dice == greenDices[index])
-      {
-         greenDicesStatus[index]=true;
-         console.log(dice);
-      }
-      else
-      {
-         greenDices[index].onclick = false;
-      }
-   }
-}
-function transitDiceBlue(e)
-{
-   console.log(e.target.id); 
-   let thisDiceIndex = 0;
-   for (let index = 0; index < blueDices.length; index++) {
-      if(event.target == blueDices[index])
-      {
-         thisDiceIndex = index;
-      }
-   }
-   let transitionValue = diceRollResults;
-   bluePos[thisDiceIndex]+=transitionValue;
-   if(bluePos[thisDiceIndex]>52)
-   bluePos[thisDiceIndex] = (bluePos[thisDiceIndex]%53)+1;
-   if(bluePos[thisDiceIndex] == 28)
+   if(next == true)
    {
-      GoHomeBlue();
-      return;
+      document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
+      document.getElementById("rollText").innerText=changeText(document.getElementById("rollText").innerText);
    }
-   let newPos = document.getElementById("b" + bluePos[thisDiceIndex]);
-   newPos.append(event.target);
-   document.getElementById("rollText").innerText = "Player 2 Turn";
-   document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
-   Turn[1] = false;
-   blueDices[thisDiceIndex].onclick = false;
-}
-function transitDiceRed()
-{
-   let thisDiceIndex = 0;
-   for (let index = 0; index < redDices.length; index++) {
-      if(event.target == redDices[index])
-      {
-         thisDiceIndex = index;
-      }
-   }
-   let transitionValue = diceRollResults;
-   redPos[thisDiceIndex]+=transitionValue;
-   if(redPos[thisDiceIndex]>52)
-   redPos[thisDiceIndex] = (redPos[thisDiceIndex]%53)+1;
-   if(redPos[thisDiceIndex] == 15)
-   {
-      GoHomeRed();
-      return;
-   }
-   let newPos = document.getElementById("b" + redPos[thisDiceIndex]);
-   newPos.append(event.target);
-   document.getElementById("rollText").innerText = "Player 3 Turn";
-   document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
-   Turn[2] = false;
-}
-function transitDiceYellow()
-{
-   let thisDiceIndex = 0;
-   for (let index = 0; index < yellowDices.length; index++) {
-      if(event.target == yellowDices[index])
-      {
-         thisDiceIndex = index;
-      }
-   }
-   let transitionValue = diceRollResults;
-   yellowPos[thisDiceIndex]+=transitionValue;
-   if(yellowPos[thisDiceIndex]>52)
-   yellowPos[thisDiceIndex] = (yellowPos[thisDiceIndex]%53)+1;
-   if(yellowPos[thisDiceIndex] == 15)
-   {
-      GoHomeYellow();
-      return;
-   }
-   let newPos = document.getElementById("b" + yellowPos[thisDiceIndex]);
-   newPos.append(event.target);
-   document.getElementById("rollText").innerText = "Player 1 Turn";
-   document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
-   Turn[4] = false;
-}
-function transitDiceGreen()
-{
-   let thisDiceIndex = 0;
-   for (let index = 0; index < greenDices.length; index++) {
-      if(event.target == greenDices[index])
-      {
-         thisDiceIndex = index;
-      }
-   }
-   let transitionValue = diceRollResults;
-   greenPos[thisDiceIndex]+=transitionValue;
-   if(greenPos[thisDiceIndex]>52)
-   greenPos[thisDiceIndex] = (greenPos[thisDiceIndex]%53)+1;
-   if(greenPos[thisDiceIndex] == 15)
-   {
-      GoHomeGreen();
-      return;
-   }
-   let newPos = document.getElementById("b" + greenPos[thisDiceIndex]);
-   newPos.append(event.target);
-   document.getElementById("rollText").innerText = "Player 4 Turn";
-   document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
-   Turn[3] = false;
-}
-function GoHomeGreen()
-{
-   console.log("Go to Home Path Now");
 
 }
-function GoHomeBlue()
+function freeDice()
 {
-   console.log("Go to Home Path Now");
+   let dice = event.target;
+   let id = getId(event.target.id);
+   let startPos = document.getElementById(id);
+   // console.log(startPos);
+   // console.log(dice);
+   startPos.append(dice);
+   document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
+   for (let index = 0; index < Dices.length; index++) {
+      if(dice == Dices[index])
+      {
+         DicesStatus[index]=true;
+      }
+      Dices[index].onclick = false;
+   }
 }
-function GoHomeYellow()
+function transitDice()
 {
-   console.log("Go to Home Path Now");
+   let thisDiceIndex = 0;
+   for (let index = 0; index < Dices.length; index++) {
+      if(event.target == Dices[index])
+      {
+         thisDiceIndex = index;
+      }
+      Dices[index].onclick = false;
+   }
+   let transitionValue = diceRollResults;
+   Pos[thisDiceIndex]+=transitionValue;
+   if(Pos[thisDiceIndex]>52)
+   Pos[thisDiceIndex] = (Pos[thisDiceIndex]%53)+1;
+   let ShouldGoHome =GoToHome(thisDiceIndex,Pos[thisDiceIndex],Pos[thisDiceIndex]-transitionValue);
+   console.log(ShouldGoHome);
+   // if(ShouldGoHome != 0)
+   // {
+   //    document.getElementById(GoHome(ShouldGoHome,Pos[thisDiceIndex])).append(event.target);
+   // }
+   // else{
+   //    document.getElementById("b" + Pos[thisDiceIndex]).append(event.target);
+   // }
+   document.getElementById("b" + Pos[thisDiceIndex]).append(event.target);
+   document.getElementsByClassName("DiceRoller")[0].onclick = DiceRoll;
+   if(transitionValue < 6)
+      document.getElementById("rollText").innerText=changeText(document.getElementById("rollText").innerText);
 }
-function GoHomeRed()
+function GoHome(DiceToGo, newPosition, e)
 {
-   console.log("Go to Home Path Now");
+   let inHomePos = newPosition - DiceToGo;
+   let GoId = "";
+   if(DiceToGo == 28) GoId ="b";
+   else if(DiceToGo == 41) GoId ="r";
+   else if(DiceToGo == 2) GoId ="g";
+   else GoId = "y";
+   if(inHomePos > 6)
+      return;
+   else
+   return (GoId + inHomePos);
+}
+//return the id of dice clicked
+function getId(e)
+{
+   let PosId = "";
+   if(e == "bdice1" || e == "bdice2" || e == "bdice3" || e == "bdice4")
+      PosId = "b30";
+   else if(e == "rdice1" || e == "rbdice2" || e == "rdice3" || e == "rdice4")
+      PosId = "b43";
+   else if(e == "gdice1" || e == "gdice2" || e == "gdice3" || e == "gdice4")
+      PosId = "b4";
+   else if(e == "ydice1" || e == "ybdice2" || e == "ydice3" || e == "ydice4")
+      PosId = "b17";
+   return PosId;
+}
+//return the index for the dices of the player whoes turn comes i.e return 0 for blue 4 for red etc.
+function getIndex(player)
+{
+   if(player == "Player 1 Turn") return 0;
+   else if(player == "Player 2 Turn") return 4;
+   else if(player == "Player 3 Turn")  return 8;
+   else return 12;
+}
+//set new player text
+function changeText(player)
+{
+   if(player == "Player 1 Turn") return "Player 2 Turn";
+   else if(player == "Player 2 Turn") return "Player 3 Turn";
+   else if(player == "Player 3 Turn")  return "Player 4 Turn";
+   else return "Player 1 Turn";
+}
+//tell if dice should go home or not
+function GoToHome(diceIndex, newPosIndex, currPosIndex)
+{
+   if(diceIndex < 4 && newPosIndex >= 28 && currPosIndex < 28) return 28;
+   else if(diceIndex < 8 && newPosIndex >= 41 && currPosIndex < 41) return 41;
+   else if(diceIndex < 12 && newPosIndex >= 2 && (currPosIndex < 2 || currPosIndex <= 52)) return 2;
+   else if(diceIndex < 16 && newPosIndex >= 15 && currPosIndex < 15) return 15;
+   return 0;
 }
